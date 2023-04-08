3c6e535bbe7f68a2a205458c70a11441390c7c45
ricardo537 <dawricardo735@gmail.com>
2023-04-08 16:42:13 +0200

    Inclusión de Main con primeras instrucciones

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Plantilla_por_turnos_fase_0;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Leer de fichero (Stephanie)
+		//Registrar Empleado (Stephanie)
+		//Hallar noches y dias (Ricardo)
+		//Completar datos de Empleado (Ricardo)
+		//Pinta mes (Ricardo)
+
+	}
+
+}
